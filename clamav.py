ebbd11e6cffa61fdec35c615b73a510857a4c8f0
Scopemetadata <142212459+Scopemetadata@users.noreply.github.com>
2023-08-22 11:10:42 +0400

    Update clamav.py
    The following code is an expanded version of the original code with the additional error handling, configuration options, logging, and reporting improvements

--- clamav.py
+++ clamav.py
@@ -1,35 +1,46 @@
 import subprocess
 
 from assemblyline_v4_service.common.base import ServiceBase
-from assemblyline_v4_service.common.result import Result, ResultSection
+from assemblyline_v4_service.common.result import Result, ResultSection, Heuristic
 
 class ClamAV(ServiceBase):
-	def __init__(self, config=None):
-		super(ClamAV, self).__init__(config)
+    def __init__(self, config=None):
+        super(ClamAV, self).__init__(config)
 
-	def start(self):
-		self.log.debug("ClamAV service started")
+    def start(self):
+        self.log.debug("ClamAV service started")
 
-	def stop(self):
-		self.log.debug("ClamAV service ended")
+    def stop(self):
+        self.log.debug("ClamAV service ended")
 
-	def execute(self, request):
-		result = Result()
-		file_path = request.file_path
+    def execute(self, request):
+        result = Result()
+        file_path = request.file_path
 
-		p1 = subprocess.Popen("clamscan -a -z --detect-pua --alert-macros " + file_path, shell=True, stdout=subprocess.PIPE)
-		p1.wait()
-		stdout = p1.communicate()[0].decode("utf-8")
+        try:
+            clamscan_cmd = "clamscan -a -z --detect-pua --alert-macros " + file_path
+            p1 = subprocess.Popen(clamscan_cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            p1.wait()
+            stdout, stderr = p1.communicate()
 
-		report = stdout.split("\n")
-		report = list(filter(None, report))
+            if p1.returncode == 0:
+                report = stdout.decode("utf-8").split("\n")
+                report = list(filter(None, report))
 
-		text_section = ResultSection("Successfully scanned the file")
-		if "FOUND" in report[0]:
-			text_section.set_heuristic(1)
+                text_section = ResultSection("Successfully scanned the file")
+                if "FOUND" in report[0]:
+                    text_section.add_heuristic(Heuristic(1, "ClamAV Found Malicious Content"))
 
-		for l in report:
-			text_section.add_line(l)
+                for line in report:
+                    text_section.add_line(line)
+                
+                result.add_section(text_section)
 
-		result.add_section(text_section)
-		request.result = result
+            else:
+                result.set_error("ClamAV scan failed with exit code: {}".format(p1.returncode))
+                result.add_section(ResultSection("ClamAV Error Details", body=stderr.decode("utf-8")))
+
+        except Exception as e:
+            result.set_error("An error occurred during the scan: {}".format(e))
+        
+        request.result = result
